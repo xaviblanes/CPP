59425f4ccd162efa8ea5c54f8062d012dee0516e
Xavi Blanes <46749418+xaviblanes@users.noreply.github.com>
2024-10-12 14:26:37 +0200

    Update matriu.cpp

--- matriu/matriu.cpp
+++ matriu/matriu.cpp
@@ -1,3 +1,9 @@
+/*****************************
+* TAD matriu d'enters En C++ *
+*                            *
+* Xavi Blanes curs 24/25     *
+*****************************/
+
 #include <iostream>
 #include <cstdlib>
 using namespace std;
@@ -6,8 +12,34 @@ class Matriu {
 private:
     int files, columnes;
     int m[10][10];
+    
 public:
-    Matriu llegirMatriu(){
+    int filesMatriu(){return this -> files;}
+
+    int columnesMatriu(){return this -> columnes;}
+
+    int elementMatriu(int i, int j){return this -> m[i][j];}
+    
+    Matriu sumarMatriu(Matriu m, Matriu n){
+       Matriu suma;
+       int i, j;
+        for(i=0;i<m.filesMatriu();i++)
+            for(j=0;j<m.columnesMatriu();j++)
+            suma.m[i][j] = m.elementMatriu(i,j) + n.elementMatriu(i,j);
+        return suma;
+    }
+
+    void escriureMatriu(){
+        int i, j;
+        for(i=0;i<this -> filesMatriu();i++){
+            for(j=0;j<this -> columnesMatriu();j++)
+            cout << elementMatriu(i,j) << ", ";
+            cout << endl;
+            }
+        return ;
+    }
+    
+        Matriu llegirMatriu(){
         Matriu matriu;
         int i, j;
         cout << "Número de files: ";
@@ -22,22 +54,17 @@ public:
             }
         return matriu;
     }
-
-    void escriureMatriu(Matriu matriu){
-        int i, j;
-        for(i=0;i<matriu.files;i++){
-            for(j=0;j<matriu.columnes;j++)
-            cout << matriu.m[i][j] << ", ";
-            cout << endl;
-            }
-        return ;
-    }
 };
 
 int main() {
-    Matriu m;
+    Matriu m, n, suma;
     m = m.llegirMatriu();
-    m.escriureMatriu(m);
+    m.escriureMatriu();
+    n = n.llegirMatriu();
+    n.escriureMatriu();
+    suma = suma.sumarMatriu(m,n);
+    cout << "La suma és: "; 
+    cout << endl;
+    suma.escriureMatriu();
     return 0;
 }
-
