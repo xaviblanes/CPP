cb748595ea46b75921ccc628267b8a9873089c7b
Xavi Blanes <46749418+xaviblanes@users.noreply.github.com>
2024-10-31 16:38:27 +0100

    Update fibonacci.cpp

--- fibonacci/fibonacci.cpp
+++ fibonacci/fibonacci.cpp
@@ -1,1 +1,34 @@
+/*****************************************************************
+*       Averigua el número de la Successió de Fibonacci          *
+*       inmediatament superior al número introduït per           *
+*       l'usuari i també quin terme de la successió és           *
+*                                                                *
+*       Xavi Blanes curs 24/25                                   *
+*****************************************************************/
 
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+using namespace std;
+
+int fibonacci(int n){
+    int aux;
+    if(n == 0) aux=0;
+    else{
+        if((n == 1)||(n == 2)) aux=1;
+        else{
+            aux=fibonacci(n-1)+fibonacci(n-2);
+        }
+    }
+    return aux;
+}
+
+int main() {
+    int n, i;
+    cout << "Introduix un número i calcularé el número de la Successió de Fibonacci inmediatament superior i també quin terme de la successió és: ";
+    cin >> n;
+    i=0;
+    while(fibonacci(i++) < n);    
+    cout << fibonacci(i-1) << " que és el terme " << i-1 << endl;
+    return 0;
+}
